0325618c1949c6a3bb8a96ffebc4e3248c475978
MaxSedachev <max.sedachev@yandex.ru>
2024-04-21 15:15:56 +0300

    implemented game Calculator

---
+++ src/games/calc.js
@@ -0,0 +1,43 @@
+import game from '../index.js';
+
+const description = 'What is the result of the expression?';
+
+const operations = ['+', '-', '*'];
+
+const calcAnswer = (num1, num2, operations) => {
+    let result
+    switch (operations) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        default:
+    }
+    return result;
+};
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+   };
+
+   const getQuestionAndAnswer = () => {
+    const num1 = getRandomInt (1, 20);
+    const num2 = getRandomInt (1, 20);
+    const operation = operations[getRandomNumber(0, operations.length)];
+    const question = `${num1}${operations}${num2}`;
+    const correctAnswer = calcAnswer(num1, num2, operation);
+
+    return [question, correctAnswer];
+   };
+
+   export default () => {
+    game(description, getQuestionAndAnswer)
+   };
